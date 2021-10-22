52f4c16c1a11d43e98ce3b1f0aa0e74a032b6f70
Rafael Cardoso <rrafa203@gmail.com>
2021-10-21 19:22:13 -0700

    Add load and save schedule function

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,26 @@ var schedules = {};
 var date = (moment().format("dddd, MMMM Do, YYYY - hh:mm:ss a"));
 
 $("#currentDay").text(date);
+
+var loadSchedules = function(){
+    schedules = JSON.parse(localStorage.getItem("schedules"));
+    if(!schedules){
+        schedules = {
+            pastSched: [],
+            presentSched: [],
+            futureSched: []
+        }
+      };
+};
+
+$(document).ready(function() {
+  $("#saveBtn").click(function(){
+      $("#textarea").val();
+  });
+});
+
+
+
+var saveSchedules = function(){
+  localStorage.setItem("schedules", JSON.stringify(schedules));
+};
